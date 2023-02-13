77da9d66ea0b92fc39c87789c1fc7ff81670bb5e
bayastec <120035666+bayastec@users.noreply.github.com>
2023-02-13 15:18:40 +0200

    findished

---
+++ calculator.js
@@ -0,0 +1,16 @@
+function addition(num,sum){
+    num = []
+    sum=0
+    for(let i = 0; i<num.lenght;$i){
+        sum=sum+num
+    } 
+    return sum
+}
+function multiplication(num,theProduct){
+    num = []
+    theProduct= 0
+     for(let i = 0; i<num.lenght;$i){
+        theProduct=theProduct*num
+    }
+    return theProduct
+}
